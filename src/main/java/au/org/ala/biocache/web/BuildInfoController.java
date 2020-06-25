194be7a29f33132f8d05b182b533975b5972b88c
Bruce Hyslop <Bruce.Hyslop@csiro.au>
2020-06-25 14:26:50 +1000

    issue #376 new BuildInfo controller and view setup template property files and maven plugin to replace build and git properties

---
+++ src/main/java/au/org/ala/biocache/web/BuildInfoController.java
@@ -0,0 +1,57 @@
+package au.org.ala.biocache.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+@Controller
+class BuildInfoController {
+
+    private final static Logger logger = Logger.getLogger(BuildInfoController.class);
+
+    @RequestMapping(value="/buildInfo", method = RequestMethod.GET)
+    public void buildInfo(Model model) {
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+        try (
+                InputStream buildInfoStream = classLoader.getResourceAsStream("/buildInfo.properties");
+                InputStream runtimeEnvironmentStream = classLoader.getResourceAsStream("/runtimeEnvironment.properties");
+            ) {
+
+            if (runtimeEnvironmentStream != null) {
+
+                try {
+                    Properties runtimeEnvironmentProperties = new Properties();
+                    runtimeEnvironmentProperties.load(runtimeEnvironmentStream);
+                    runtimeEnvironmentProperties.setProperty("java.version", System.getProperty("java.version"));
+                    model.addAttribute("runtimeEnvironment", runtimeEnvironmentProperties);
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
+
+            if (buildInfoStream != null) {
+
+                try {
+                    Properties buildInfoProperties = new Properties();
+                    buildInfoProperties.load(buildInfoStream);
+                    model.addAttribute("buildInfo", buildInfoProperties);
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
+
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+}
--- src/main/webapp/WEB-INF/decorators/main.jsp
+++ src/main/webapp/WEB-INF/decorators/main.jsp
@@ -51,6 +51,8 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <decorator:head />
 </head>
 <body>
 
