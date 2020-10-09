19bddd61e64deac81cdc7b1e4a85b181c7269e20
Bruce Hyslop <bruce.hyslop@csiro.au>
2020-10-09 17:38:56 +1100

    #514 added BlockingQueue to support backpressure

--- src/test/java/au/org/ala/biocache/service/LoggerServiceSpec.groovy
+++ src/test/java/au/org/ala/biocache/service/LoggerServiceSpec.groovy
@@ -31,6 +31,78 @@ class LoggerServiceSpec extends Specification {
         1 * loggerService.restTemplate.postForEntity(_, new HttpEntity<>(logEvent, headers), Void) >> { new ResponseEntity<Void>(HttpStatus.OK) }
     }
 
+    void 'block on queue limit'() {
+
+        setup:
+        LoggerRestService loggerService = new LoggerRestService()
+        loggerService.enabled = false
+        loggerService.restTemplate = Stub(RestOperations) {
+            postForEntity(_, _, Void) >> { sleep(11); new ResponseEntity<Void>(HttpStatus.OK) }
+        }
+        loggerService.eventQueueSize = 10
+//        loggerService.throttleDelay = 100
+
+        loggerService.init()
+
+        boolean logEventBlocked = true
+
+        when: 'log more then buffer in quick succession'
+        10.times {
+            LogEventVO logEvent = new LogEventVO()
+            logEvent.sourceUrl = it as String
+
+            loggerService.logEvent(logEvent)
+        }
+
+        then:
+        true
+//        0 * loggerService.restTemplate.postForEntity(_, _, Void) >> { sleep(11); new ResponseEntity<Void>(HttpStatus.OK) }
+
+        when:
+        Thread.start {
+
+            sleep(100)
+
+            LogEventVO logEvent = new LogEventVO()
+            logEvent.sourceUrl = 'blocked 11'
+
+            loggerService.logEvent(logEvent)
+            logEventBlocked = false
+        }
+
+        then:
+        logEventBlocked
+//        _ * loggerService.restTemplate.postForEntity(*_) >> { sleep(12); new ResponseEntity<Void>(HttpStatus.OK) }
+
+        when:
+        sleep(10000)
+
+        then: 'expect all events POSTed'
+        !logEventBlocked
+//        _ * loggerService.restTemplate.postForEntity(*_) >> { sleep(12); new ResponseEntity<Void>(HttpStatus.OK) }
+
+
+//        when:
+//        logEventBlocked = true
+//        Thread.start({
+//            loggerService.logEvent(logEvent)
+//            logEventBlocked = false
+//        })
+//
+//        then:
+//        logEventBlocked
+//        loggerService.eventQueue.size() == 9
+
+//        when:
+//        sleep(2000)
+//
+//        then:
+//        10 * loggerService.restTemplate.postForEntity(_, new HttpEntity<>(logEvent, headers), Void) >> { new ResponseEntity<Void>(HttpStatus.OK) }
+//        !logEventBlocked
+
+//        then: 'expect all events POSTed'
+    }
+
     void 'thottle log events'() {
 
         setup:
@@ -41,22 +113,17 @@ class LoggerServiceSpec extends Specification {
 
         loggerService.init()
 
-        LogEventVO logEvent = new LogEventVO()
-        HttpHeaders headers = new HttpHeaders()
-        headers.set(HttpHeaders.USER_AGENT, logEvent.getUserAgent())
-
         when: 'log more then buffer in quick succession'
         10.times {
+            LogEventVO logEvent = new LogEventVO()
+            logEvent.sourceUrl = it as String
+
             loggerService.logEvent(logEvent)
         }
-
-        then:
-        0 * loggerService.restTemplate.postForEntity(_, new HttpEntity<>(logEvent, headers), Void) >> { new ResponseEntity<Void>(HttpStatus.OK) }
-
-        when:
-        Thread.sleep(10 * loggerService.throttleDelay)
+        
+        sleep(10 * loggerService.throttleDelay)
 
         then: 'expect all events POSTed'
-        10 * loggerService.restTemplate.postForEntity(_, new HttpEntity<>(logEvent, headers), Void) >> { new ResponseEntity<Void>(HttpStatus.OK) }
+        10 * loggerService.restTemplate.postForEntity(*_) >> { new ResponseEntity<Void>(HttpStatus.OK) }
     }
 }
