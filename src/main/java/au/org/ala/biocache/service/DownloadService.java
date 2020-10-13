decd14ee8615d11af253f1ef3f4224e34ebcd550
alexhuang091 <HUA091@csiro.au>
2020-10-13 16:28:31 +1100

    pass profile name into doi so it can be shown to end users

--- src/main/java/au/org/ala/biocache/service/DownloadService.java
+++ src/main/java/au/org/ala/biocache/service/DownloadService.java
@@ -708,6 +708,7 @@ public class DownloadService implements ApplicationListener<ContextClosedEvent> 
                             doiDetails.setLicence(licence);
                             doiDetails.setQueryTitle(requestParams.getDisplayString());
                             doiDetails.setApplicationMetadata(requestParams.getDoiMetadata());
+                            doiDetails.setDataProfile(requestParams.getQualityProfile());
                             doiDetails.setQualityFilters(qualityFilters);
                             doiDetails.setDisplayTemplate(requestParams.getDoiDisplayTemplate());
 
