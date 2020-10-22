c5294f84f88b0237c3bc2ea63543b186f9bf44ff
Bruce Hyslop <bruce.hyslop@csiro.au>
2020-10-22 15:43:10 +1100

    #524 added extra units tests of boundary conditions

--- src/test/java/au/org/ala/biocache/service/ListsServiceSpec.groovy
+++ src/test/java/au/org/ala/biocache/service/ListsServiceSpec.groovy
@@ -2,27 +2,48 @@ package au.org.ala.biocache.service
 
 import au.org.ala.biocache.dto.Kvp
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class ListsServiceSpec extends Specification {
 
     ListsService listService = new ListsService()
 
-    def 'find Kpv'() {
+    @Unroll
+    def 'find Kpv(lft: #lft, rgt: #rgt)'() {
 
         setup:
         List<Kvp> kvps = [
-                new Kvp(1, 1),
-                new Kvp(2, 2),
-                new Kvp(3, 3)
+                new Kvp(10, 10),
+                new Kvp(20, 20),
+                new Kvp(24, 26),
+                new Kvp(30, 30)
         ]
 
 
-        Kvp lftrgt = new Kvp(4, 4)
-
         when:
-        Kvp result = listService.find(kvps, lftrgt)
+        Kvp result = listService.find(kvps, new Kvp(lft, rgt))
 
         then:
-        result == null
+        if (expectedKvp != null) {
+
+            result.lft == expectedKvp.lft
+            result.rgt == expectedKvp.rgt
+
+        } else {
+            result == null
+        }
+
+        where:
+        lft | rgt || expectedKvp
+        1   | 1   || null
+        10  | 10  || new Kvp(10, 10)
+        19  | 21  || null
+        24  | 26  || new Kvp(24, 26)
+        25  | 25  || null
+        29  | 30  || null
+        30  | 30  || new Kvp(30, 30)
+        30  | 31  || null
+        40  | 40  || null
+
     }
 }
