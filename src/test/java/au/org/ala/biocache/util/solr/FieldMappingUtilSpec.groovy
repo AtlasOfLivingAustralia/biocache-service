f45953e147a5721e390157d5a7a56276bfb62e91
Bruce Hyslop <bruce.hyslop@csiro.au>
2021-05-21 13:17:05 +1000

    issue #354 fixed query parse regex translateQuery uses translateFieldName (remove duplication)

--- src/test/java/au/org/ala/biocache/util/solr/FieldMappingUtilSpec.groovy
+++ src/test/java/au/org/ala/biocache/util/solr/FieldMappingUtilSpec.groovy
@@ -26,7 +26,10 @@ class FieldMappingUtilSpec extends Specification {
         where:
         query || expetedQuery
         'taxon_name:*' || 'scientificName:*'
+        '(-month:"08")' || '(-month:"08")'
+        '-(month:"08")' || '-(month:"08")'
         '-taxon_name:*' || '-scientificName:*'
+        'taxon_name:*  common_name:"test"' || 'scientificName:*  vernacularName:"test"'
         'taxon_name:* AND -(common_name:"test")' || 'scientificName:* AND -(vernacularName:"test")'
         'deleted:*' || 'deprecated_deleted:*'
         'assertions:*' || 'assertions:*'
@@ -34,6 +37,7 @@ class FieldMappingUtilSpec extends Specification {
         'assertions:(badlyFormedBasisOfRecord coordinatesOutOfRange)' || 'assertions:(BASIS_OF_RECORD_INVALID COORDINATE_OUT_OF_RANGE)'
         'taxon_name:* assertions:badlyFormedBasisOfRecord AND -(common_name:"test")' || 'scientificName:* assertions:BASIS_OF_RECORD_INVALID AND -(vernacularName:"test")'
         '-(taxon_name:* AND -taxon_name:*)' || '-(scientificName:* AND -scientificName:*)'
+        'taxon_name:* assertions:"badlyFormedBasisOfRecord" AND -(common_name:"test")' || 'scientificName:* assertions:"BASIS_OF_RECORD_INVALID" AND -(vernacularName:"test")'
     }
 
     def 'translateFieldList'() {
