5b208dab68571d66910e759fa8af17197bfad780
Bruce Hyslop <bruce.hyslop@csiro.au>
2022-01-28 17:11:56 +1100

    AtlasOfLivingAustralia/api-gateway#5 `@Secured` annotations not working

--- src/main/java/au/org/ala/biocache/config/SecurityConfig.java
+++ src/main/java/au/org/ala/biocache/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package au.org.ala.biocache.config;
 import au.org.ala.ws.security.AlaWebServiceAuthFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import javax.inject.Inject;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @Order(1)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
