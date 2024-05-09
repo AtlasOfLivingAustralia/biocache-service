768698b27cbac742bcc644d71637f3f081495e08
Adam Collins <adam.collins@csiro.au>
2024-05-09 15:10:48 +1000

    Change @PropertySource to spring.config.import

--- src/main/java/au/org/ala/biocache/config/Application.java
+++ src/main/java/au/org/ala/biocache/config/Application.java
@@ -15,7 +15,6 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @ImportResource("classpath:spring.xml")
 @ComponentScan( { "au.org.ala.biocache.*" , "au.org.ala.ws.config" })
-@PropertySource(value="file:///data/biocache/config/biocache-config.properties", ignoreResourceNotFound=true)
 public class Application extends SpringBootServletInitializer {
 
     @Override
