913661d9fbffbb5f420d90e716bc4ecafa5435e1
h-m-jones <h.manders.jones@nbn.org.uk>
2025-06-27 12:33:24 +0100

    FIX - PT-97: Download facet results for Month doesn't include facet values https://nbnatlas.atlassian.net/browse/PT-97
    Will also issue PR with ALA

--- src/main/java/au/org/ala/biocache/stream/StreamFacet.java
+++ src/main/java/au/org/ala/biocache/stream/StreamFacet.java
@@ -91,11 +91,11 @@ public class StreamFacet implements ProcessInterface {
     public boolean process(Tuple tuple) {
         String name = null;
         Long count = 0L;
-        for (Object value : tuple.getMap().values()) {
-            if (value instanceof String) {
-                name = (String) value;
+        for (Object o : tuple.getMap().values()) {
+            if (o instanceof Long) {
+                count = (Long) o;
             } else {
-                count = (Long) value;
+                name = String.valueOf(o);
             }
         }
 
