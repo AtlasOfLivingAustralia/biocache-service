4ee3770d874f7284867a5322b0898b966bccd575
Adam Collins <adam.collins@csiro.au>
2026-01-27 13:22:04 +1000

    #970 update swagger-ui defaults

--- src/main/java/au/org/ala/biocache/config/Application.java
+++ src/main/java/au/org/ala/biocache/config/Application.java
@@ -1,21 +1,32 @@
 package au.org.ala.biocache.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.SwaggerUiConfigProperties;
+import org.springdoc.core.SwaggerUiOAuthProperties;
+import org.springdoc.webmvc.ui.SwaggerIndexPageTransformer;
+import org.springdoc.webmvc.ui.SwaggerIndexTransformer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.resource.ResourceTransformerChain;
+import org.springframework.web.servlet.resource.TransformedResource;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @SpringBootApplication
 @ImportResource("classpath:spring.xml")
 @ComponentScan( { "au.org.ala.biocache.*" , "au.org.ala.ws.config" })
 public class Application extends SpringBootServletInitializer {
+
+    @Value("${server.servlet.context-path:}")
+    private String contextPath;
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -35,6 +46,28 @@ public class Application extends SpringBootServletInitializer {
                                 .version(appVersion)
                                 .description(appDescription)
                                 .termsOfService(appTermsUrl));
+    }
+
+    @Bean
+    public SwaggerIndexTransformer customIndexTransformer(SwaggerUiConfigProperties swaggerUiConfig, SwaggerUiOAuthProperties swaggerUiOAuthProperties, ObjectMapper objectMapper) {
+
+        return new SwaggerIndexPageTransformer(swaggerUiConfig, swaggerUiOAuthProperties, objectMapper) {
+
+            @Override
+            public Resource transform(HttpServletRequest request, Resource resource, ResourceTransformerChain transformerChain) throws IOException {
+                Resource transformedResource = super.transform(request, resource, transformerChain);
+                String html = new String(transformedResource.getInputStream().readAllBytes());
+                String updatedHtml = overwriteSwaggerDefaultUrl(html);
+                return new TransformedResource(transformedResource, updatedHtml.getBytes());
+            }
+
+            @Override
+            protected String overwriteSwaggerDefaultUrl(String html) {
+                // replaces petstore url with our api-docs url
+                return html.replace("url: \"https://petstore.swagger.io/v2/swagger.json\"",
+                        "url: \"" + contextPath + "/v3/api-docs\"");
+            }
+        };
     }
 
     public static void main(String[] args) {
